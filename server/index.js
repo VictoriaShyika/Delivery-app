1d9327d4bded10b635f7763e9c374accbdc48374
VictoriaShyika <victoriashyika@gmail.com>
2022-07-22 20:53:09 +0300

    pushing to heroku 3

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import { deliveryData } from "./data.js";
 import path from "path";
 import cors from "cors";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
 app.use(cors());
@@ -9,6 +11,9 @@ app.use(cors());
 app.get("/data", function (req, res) {
   res.send(deliveryData);
 });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
